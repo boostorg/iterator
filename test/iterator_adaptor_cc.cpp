1574946e1324020d9d47a26fc90bf72d4587a4ce
Jeremy Siek <jeremy.siek@gmail.com>
2002-10-23 22:57:24 +0000

    new test
    [SVN r610]

---
+++ test/iterator_adaptor_cc.cpp
@@ -0,0 +1,25 @@
+#include <boost/iterator/iterator_adaptors.hpp>
+#include <boost/iterator/iterator_concepts.hpp>
+#include <boost/concept_check.hpp>
+#include <boost/cstdlib.hpp>
+#include <list>
+
+int main()
+{
+  {
+    typedef boost::reverse_iterator<int*, int, int&, int*, 
+      boost::mutable_lvalue_iterator_tag, boost::random_access_traversal_tag, std::ptrdiff_t> rev_iter;
+    boost::function_requires< boost_concepts::MutableLvalueIteratorConcept<rev_iter> >();
+    boost::function_requires< boost_concepts::RandomAccessTraversalConcept<rev_iter> >();
+    boost::function_requires< boost::RandomAccessIteratorConcept<rev_iter> >();
+  }
+  {
+    typedef boost::reverse_iterator<std::list<int>::const_iterator, int, const int&, const int*, 
+      boost::constant_lvalue_iterator_tag, boost::bidirectional_traversal_tag, std::ptrdiff_t> rev_iter;
+    boost::function_requires< boost_concepts::ConstantLvalueIteratorConcept<rev_iter> >();
+    boost::function_requires< boost_concepts::BidirectionalTraversalConcept<rev_iter> >();
+    boost::function_requires< boost::BidirectionalIteratorConcept<rev_iter> >();
+  }
+
+  return boost::exit_success;
+}
