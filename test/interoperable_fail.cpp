c068ca0bdaf17143b07bc0bfb1d3fe72268ff48d
Thomas Witt <witt@acm.org>
2002-12-09 18:07:16 +0000

    Added failure checks.
    [SVN r754]

---
+++ test/interoperable_fail.cpp
@@ -0,0 +1,16 @@
+#include <boost/iterator/iterator_adaptors.hpp>
+#include <boost/concept_check.hpp>
+#include <boost/cstdlib.hpp>
+#include <list>
+
+int main()
+{
+  {
+    typedef boost::reverse_iterator<std::list<int*>::iterator>   rev_iter;
+    typedef boost::indirect_iterator<std::list<int*>::iterator>  ind_iter;
+
+    ind_iter() == rev_iter();
+  }
+
+  return boost::exit_success;
+}
---
+++ test/is_convertible_fail.cpp
@@ -0,0 +1,15 @@
+#include <boost/iterator/iterator_adaptors.hpp>
+#include <boost/static_assert.hpp>
+#include <boost/cstdlib.hpp>
+
+int main()
+{
+  {
+    typedef boost::reverse_iterator<int*>  rev_iter1;
+    typedef boost::reverse_iterator<char*> rev_iter2;
+
+    BOOST_STATIC_ASSERT((boost::is_convertible<rev_iter1, rev_iter2>::value));
+  }
+
+  return boost::exit_success;
+}
