22670777b806f66af0b3eb95581b3bb62b8f1db1
Dave Abrahams <dave@boostpro.com>
2003-11-19 22:03:48 +0000

    Change the way input_output_iterator_tag is convertible to output_iterator_tag from inheritance to implicit conversion operator, to avoid some tag dispatch ambiguities.
    [SVN r20868]

--- include/boost/iterator/detail/facade_iterator_category.hpp
+++ include/boost/iterator/detail/facade_iterator_category.hpp
@@ -37,7 +37,19 @@ namespace boost { struct use_default; }
 namespace boost { namespace detail  {
 
 struct input_output_iterator_tag
-  : std::input_iterator_tag, std::output_iterator_tag {};
+  : std::input_iterator_tag
+{
+    // Using inheritance for only input_iterator_tag helps to avoid
+    // ambiguities when a stdlib implementation dispatches on a
+    // function which is overloaded on both input_iterator_tag and
+    // output_iterator_tag, as STLPort does, in its __valid_range
+    // function.  I claim it's better to avoid the ambiguity in these
+    // cases.
+    operator std::output_iterator_tag() const
+    {
+        return std::output_iterator_tag();
+    }
+};
 
 //
 // True iff the user has explicitly disabled writability of this
