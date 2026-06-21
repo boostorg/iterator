286c9885d655c15e27107542ee66852322b50ffb
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2026-06-22 00:04:10 +0900

    Constrain distance function using is_iterator type trait (#96)
    * Constrain distance function using is_iterator type trait
    
    * Add enable_if include
    
    * Fix template syntax in distance function
    
    * Fix syntax issue in distance enable_if
    
    * Test which overload of distance is called
    
    Added a test for custom distance function using Foo struct.
    
    * Fix wrong spelling of std::ptrdiff_t
    
    * Move Foo struct to global scope
    
    * Andrey magic
    
    * Fix headers, add copyright
    
    * Constrain advance to iterators
    
    * Test contraint of advance function with int overload
    
    Added overload for advance function to handle integers.
    
    * Fix enable_if condition for advance function
    
    * Fix template syntax in advance constraint
    
    * Fix advance test
    
    * Remove constexpr specifier
    
    * Make advance overload require conversion in parameter
    
    * Explain choice of type long for n parameter

--- include/boost/iterator/distance.hpp
+++ include/boost/iterator/distance.hpp
@@ -1,4 +1,5 @@
 // Copyright (C) 2017 Michel Morin.
+// Copyright (C) 2026 Jeremy W. Murphy
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
@@ -8,8 +9,11 @@
 #define BOOST_ITERATOR_DISTANCE_HPP
 
 #include <boost/config.hpp>
+#include <boost/iterator/is_iterator.hpp>
 #include <boost/iterator/iterator_categories.hpp>
 #include <boost/iterator/iterator_traits.hpp>
+
+#include <type_traits>
 
 namespace boost {
 namespace iterators {
@@ -40,7 +44,11 @@ distance_impl(RandomAccessIterator first, RandomAccessIterator last, random_acce
 namespace distance_adl_barrier {
 
 template< typename SinglePassIterator >
-inline BOOST_CXX14_CONSTEXPR typename iterator_difference< SinglePassIterator >::type
+inline BOOST_CXX14_CONSTEXPR
+typename std::enable_if<
+    boost::is_iterator< SinglePassIterator >::value,
+    iterator_difference< SinglePassIterator >
+>::type::type
 distance(SinglePassIterator first, SinglePassIterator last)
 {
     return detail::distance_impl(first, last, typename iterator_traversal< SinglePassIterator >::type());
